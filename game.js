ea7c9113d0fd62fdd904a07266342feecd61a8f4
Farah Sallam <farahsallam@student.olympic.edu>
2024-04-28 11:31:55 -0700

    added h1 to index.html, added first style rule, create a functin in game.js

---
+++ game.js
@@ -0,0 +1,16 @@
+function getComputerChoice 
+{
+    const randomNumber = Math.random();
+    if (randomNumber < 0.33) 
+    {
+        return "rock";
+    } 
+    else if (randomNumber < 0.66) 
+    {
+        return "paper";
+    } 
+    else 
+    {
+        return "scissors";
+    }
+}
